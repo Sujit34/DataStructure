c279ab24a39e9403b7dab450ffd25a555c69091c
sujit34 <sujitcsecuet@gmail.com>
2023-01-08 14:37:50 -0600

    BinarySearchOnSortedAndRotatedArray

--- src/Miscellaneous/BinarySearchOnSortedAndRotatedArray.java
+++ src/Miscellaneous/BinarySearchOnSortedAndRotatedArray.java
@@ -4,10 +4,11 @@ public class BinarySearchOnSortedAndRotatedArray {
 
 	public static int pivotedBinarySearch(int arr[], int target) {
 		int pivotIndex = findPivot(arr, 0, arr.length - 1);
-		if (arr[pivotIndex] == target)
-			return pivotIndex;
+
 		if (pivotIndex == -1)
 			return binarySearch(arr, 0, arr.length - 1, target);
+		if (arr[pivotIndex] == target)
+			return pivotIndex;
 		if (arr[0] <= target)
 			return binarySearch(arr, 0, pivotIndex - 1, target);
 		return binarySearch(arr, pivotIndex + 1, arr.length - 1, target);
@@ -55,19 +56,19 @@ public class BinarySearchOnSortedAndRotatedArray {
 }
 
 /*
- 
- Follow the steps mentioned below to implement the idea:  
-
--Find out the pivot point using binary search. We will set the low pointer as the first array index and high with the last array index.
-	-From the high and low we will calculate the mid value. 
-	-If the value at mid-1 is greater than the one at mid, return that value as the pivot.
-	-Else if the value at the mid+1 is less than mid, return mid value as the pivot.
-	-Otherwise, if the value at low position is greater than mid position, consider the left half. Otherwise, consider the right half.
--Divide the array into two sub-arrays based on the pivot that was found.
--Now call binary search for one of the two sub-arrays.
-	-If the element is greater than the 0th element then search in the left array
-	-Else search in the right array.
--If the element is found in the selected sub-array then return the index
--Else return -1.
-
+ * 
+ * Follow the steps mentioned below to implement the idea:
+ * 
+ * -Find out the pivot point using binary search. We will set the low pointer as
+ * the first array index and high with the last array index. -From the high and
+ * low we will calculate the mid value. -If the value at mid-1 is greater than
+ * the one at mid, return that value as the pivot. -Else if the value at the
+ * mid+1 is less than mid, return mid value as the pivot. -Otherwise, if the
+ * value at low position is greater than mid position, consider the left half.
+ * Otherwise, consider the right half. -Divide the array into two sub-arrays
+ * based on the pivot that was found. -Now call binary search for one of the two
+ * sub-arrays. -If the element is greater than the 0th element then search in
+ * the left array -Else search in the right array. -If the element is found in
+ * the selected sub-array then return the index -Else return -1.
+ * 
  */
